35886d8ac3fb0a91dd2f1d2a6cc4167460bff71d
seyyah <nurettin.senyer@gmail.com>
2025-11-08 20:23:17 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/ilhankarabicak_web_sayfasi_v2/' : '/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  define: {
-    'import.meta.env.BASE_URL': JSON.stringify(process.env.NODE_ENV === 'production' ? '/ilhankarabicak_web_sayfasi_v2/' : '/'),
   },
 });
